d3273250ff1c9acb1a661462b67f79867df1fa4b
Mohammad Reza <hoznaghashi@gmail.com>
2023-10-28 19:49:12 +0000

    changing architechture to spatio temporal attention

--- bulid_transformer_model.py
+++ bulid_transformer_model.py
@@ -6,19 +6,41 @@ from tensorflow import keras
 from keras import layers
 from keras.models import Model
 
-class Tublet_projection(keras.layers.Layer):
-    def __init__(self,patch_size,embed_dim,**kwargs):
+class Tublet_projection(layers.Layer):
+    def __init__(self, embed_dim, patch_size, **kwargs):
         super().__init__(**kwargs)
-        self.projection = layers.Conv3D(
-                                        filters=embed_dim,
-                                        kernel_size=patch_size,
-                                        strides=patch_size,
-                                        padding="VALID")
-        self.flatten = layers.Reshape(target_shape=(-1, embed_dim))
+        self.patch_size = patch_size
+        self.embed_dim = embed_dim
+    def build(self,input_shape):
+
+        self.projection = layers.Conv2D(
+            filters=self.embed_dim,
+            kernel_size=self.patch_size,
+            strides=self.patch_size,
+            padding="VALID",
+        )
+        self.flatten = layers.Reshape(target_shape=(input_shape[1],-1, self.embed_dim))
+
     def call(self, videos):
         projected_patches = self.projection(videos)
         flattened_patches = self.flatten(projected_patches)
         return flattened_patches
+        
+class ModulationEmbedding(layers.Layer):
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+    def build(self, input_shape):
+        _, num_tokens,embed_dim= input_shape
+        self.position_embedding = layers.Embedding(input_dim=num_tokens, output_dim= embed_dim)
+        self.speed_embedding = layers.Embedding(input_dim = num_tokens, output_dim= embed_dim)
+        self.positions = tf.range(start=0, limit=num_tokens, delta=1)
+    def call(self, encoded_tokens,runing_speed):
+        # Encode the positions and add it to the encoded tokens
+        encoded_positions = self.position_embedding(self.positions)
+        encoded_speed = self.speed_embedding(runing_speed)
+        x = encoded_tokens + encoded_speed
+        encoded_tokens = encoded_tokens + encoded_speed + encoded_positions
+        return 
 
 class PositionalEncoder(layers.Layer):
     def __init__(self, embed_dim, **kwargs):
@@ -38,25 +60,30 @@ class PositionalEncoder(layers.Layer):
         encoded_tokens = encoded_tokens + encoded_positions
         return encoded_tokens
 
-def bulid_model(num_heads,num_layers,delay,embed_dim,output_shape):
+def bulid_model(num_heads,spatial_layers,temporal_layers,delay,embed_dim,output_shape):
     delay = delay
     num_heads =num_heads
-    transformer_layers = num_layers
+    num_spatial_tranformers = spatial_layers
+    num_temporal_transfomers = temporal_layers
     embed_dim = embed_dim
     input_shape = (delay,304,608,1)
     LAYER_NORM_EPS = 1e-6
     output_shape = output_shape
+
+
+
     inputs = layers.Input(shape=input_shape)
+    runing_speed = layers.Input(shape = (delay))
     # Create patches.
     patches = Tublet_projection(patch_size=(delay,10,10),embed_dim=embed_dim)(inputs)
     pathces = Lambda(lambda x : x/255.0)(patches)
     # Encode patches.
-    encoded_patches = PositionalEncoder(embed_dim=embed_dim)(patches)
-    for _ in range(transformer_layers):
+    encoded_patches = PositionalEncoder(embed_dim=embed_dim)(pathces)
+    for _ in range(num_spatial_tranformers):
         # Layer normalization and MHSA
         x1 = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
         attention_output = layers.MultiHeadAttention(
-            num_heads=num_heads, key_dim=embed_dim // num_heads, dropout=0.1
+            num_heads=num_heads, key_dim=embed_dim // num_heads, dropout=0.1,attention_axes=-2
         )(x1, x1)
 
         # Skip connection
@@ -73,6 +100,34 @@ def bulid_model(num_heads,num_layers,delay,embed_dim,output_shape):
 
         # Skip connection
         encoded_patches = layers.Add()([x3, x2])
+
+    encoded_patches = layers.Reshape((delay,-1))(encoded_patches)
+    encoded_patches = ModulationEmbedding()(encoded_patches,runing_speed)
+
+    for _ in range(num_spatial_tranformers):
+        # Layer normalization and MHSA
+        x1 = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
+        attention_output = layers.MultiHeadAttention(
+            num_heads=num_heads, key_dim=embed_dim // num_heads, dropout=0.1,attention_axes=-2
+        )(x1, x1)
+
+        # Skip connection
+        x2 = layers.Add()([attention_output, encoded_patches])
+
+        # Layer Normalization and MLP
+        x3 = layers.LayerNormalization(epsilon=1e-6)(x2)
+        x3 = keras.Sequential(
+            [
+                layers.Dense(units=embed_dim * 4, activation=tf.nn.gelu),
+                layers.Dense(units=embed_dim, activation=tf.nn.gelu),
+            ]
+        )(x3)
+
+        # Skip connection
+        encoded_patches = layers.Add()([x3, x2])
+
+
+
     representation = layers.LayerNormalization(epsilon=LAYER_NORM_EPS)(encoded_patches)
     representation = layers.GlobalAvgPool1D()(representation)
     outputs = layers.Dense(units=output_shape, activation="linear")(representation)
